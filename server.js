23aaeecd4027e23c287de2266dbffd9f328f50e1
nancyyli <lixiang@ccs.neu.edu>
2018-08-04 18:44:29 -0400

    started node with mongoose

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var app = express()
+var session = require('express-session')
+var bodyParser = require('body-parser')
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'any string'
+}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/webdev-summer2');
+
+const userService = require('./services/user.service.server'); //(app);
+userService(app);
+
+// require('./services/section.service.server')(app);
+
+app.listen(3000)
