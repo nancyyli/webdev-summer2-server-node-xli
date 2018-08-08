2da73e48f379613f6ee03a68a656b01db3726918
nancyyli <lixiang@ccs.neu.edu>
2018-08-07 23:12:30 -0400

    changed allow cross origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://frozen-waters-79015.herokuapp.com/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
